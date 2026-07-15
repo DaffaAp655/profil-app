27e8472e6647b0ab7710f500f8ca8858425d0f26
Daffa Ap <daffaap655@gmail.com>
2026-07-15 19:43:27 +0700

    Add: CRUD mahasiswa - Resource Controller, form, validasi

--- app/Http/Controllers/TentangController.php
+++ app/Http/Controllers/TentangController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class tentangController extends Controller
+class TentangController extends Controller
 {
     public function index()
     {
